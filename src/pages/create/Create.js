9b38bad116e12f8376ca65e86f884e28c556df9f
Chirag Jain <cjain8060@gmail.com>
2022-01-19 20:21:54 +0530

    added a create new project form to create page

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,9 +1,64 @@
+import { useState } from "react";
+
+/* STYLES */
 import "./Create.css";
 
 const Create = () => {
+  // form field values
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [assignedUsers, setAssignedUsers] = useState([]);
+  const [formError, setFormError] = useState(null);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(name, details, dueDate, category, assignedUsers);
+  };
   return (
-    <div>
-      <h1>Create</h1>
+    <div className="create-form">
+      <h2 className="page-title">Create a new Project</h2>
+      <form onSubmit={submitHandler}>
+        <label>
+          <span>Project name:</span>
+          <input
+            required
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Project Details:</span>
+          <textarea
+            required
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
+          ></textarea>
+        </label>
+        <label>
+          <span>Set due date:</span>
+          <input
+            required
+            type="date"
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+          />
+        </label>
+        <label>
+          <span>Project category:</span>
+          {/* add select here later */}
+        </label>
+        <label>
+          <span>Assign to:</span>
+          {/* add select here later */}
+        </label>
+
+        <button className="btn">Add Project</button>
+
+        {formError && <p className="error">{formError}</p>}
+      </form>
     </div>
   );
 };
