b003d1e40a00f1761162f80bd4a77b57e7016730
Chirag Jain <cjain8060@gmail.com>
2022-01-19 23:47:01 +0530

    added project filter ui on the main dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -2,6 +2,7 @@
 import { useCollection } from "../../hooks/useCollection";
 
 /* COMPONENTS & STYLES */
+import ProjectFilter from "./ProjectFilter";
 import ProjectList from "../../components/ProjectList/ProjectList";
 import "./Dashboard.css";
 
@@ -12,6 +13,7 @@ const Dashboard = () => {
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
+      {documents && <ProjectFilter />}
       {documents && <ProjectList projects={documents} />}
     </div>
   );
