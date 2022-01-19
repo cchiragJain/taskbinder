50f01679e691e8140f4989bd5b045609e8c0384a
Chirag Jain <cjain8060@gmail.com>
2022-01-19 22:29:36 +0530

    added a live project details page using useDocument hook

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,9 +1,25 @@
+import { useParams } from "react-router-dom";
+
+/* HOOKS */
+import { useDocument } from "../../hooks/useDocument";
+
+/* COMPONENETS & STYLES */
 import "./Project.css";
 
 const Project = () => {
+  const { id } = useParams();
+  const { document, error } = useDocument("projects", id);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+  if (!document) {
+    return <div className="loading">Loading...</div>;
+  }
+
   return (
-    <div>
-      <h1>Project</h1>
+    <div className="project-details">
+      <h1>{document.name}</h1>
     </div>
   );
 };
